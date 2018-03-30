466fdccec571c9c41872747fddf523a369c50c07
raina4 <raina4@illinois.edu>
2018-03-29 22:29:04 -0500

    initial revision fulcrum scheduler algorithm

---
+++ src/global_scheduler/fulcrum_scheduler_algorithm.cc
@@ -0,0 +1,23 @@
+#include "fulcrum_scheduler_algorithm.h"
+
+/**
+ * Checks if the given local scheduler satisfies the task's hard constraints.
+ *
+ * @param scheduler Local scheduler.
+ * @param spec Task specification.
+ * @return True if all tasks's resource constraints are satisfied. False
+ *         otherwise.
+ */
+void refreshLsLoadMap() {
+
+}
+
+void updateLsLoadMap() {
+
+}
+
+DBClientID findLsByWeightedLoad() {
+
+}
+
+
