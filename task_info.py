bfc97c6933cf8e4479a5e2150e89b410b5e5a970
Samarth Kulshreshtha <smkuls@gmail.com>
2018-04-01 15:06:24 -0500

    Correcting arithmetic error in subtracting times

--- task_info.py
+++ task_info.py
@@ -16,8 +16,8 @@ task_profiles = ray.global_state.task_profiles(num_tasks)
 task_durations = []
 
 for task_profile in task_profiles.items():
-    task_durations.append(task_profile[1]['get_arguments_start'] -
-                          task_profile[1]["store_outputs_end"])
+    task_durations.append(task_profile[1]["store_outputs_end"] -
+                          task_profile[1]['get_arguments_start'])
 
 with open(output_file_name, "w+") as file_handle:
     file_handle.write("\n".join(map(str, task_durations)))
