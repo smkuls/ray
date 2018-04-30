1c80447d93c6eb14b8550e799da63fa6ec61a856
Samarth Kulshreshtha <smkuls@gmail.com>
2018-04-29 20:53:28 -0500

    Switiching back to Ray's schduler

--- src/global_scheduler/global_scheduler_algorithm.cc
+++ src/global_scheduler/global_scheduler_algorithm.cc
@@ -361,9 +361,9 @@ bool handle_task_waiting(GlobalSchedulerState *state,
                          GlobalSchedulerPolicyState *policy_state,
                          Task *task) {
   RAY_LOG(INFO)<<"Global Scheduler handle_task_waiting invoked \n";
-  //return handle_task_waiting_random(state, policy_state, task);
+  return handle_task_waiting_random(state, policy_state, task);
   // return handle_task_waiting_fulcrum(state, policy_state, task);
-  return handle_task_waiting_sparrow(state, policy_state, task);
+  // return handle_task_waiting_sparrow(state, policy_state, task);
 }
 
 void handle_object_available(GlobalSchedulerState *state,
