4ba67a14be36c1b5c3ca8e936f2130ae7e67669a
raina4 <raina4@illinois.edu>
2018-05-06 15:11:49 -0500

    fixes to get stats file

--- scripts/get_task_stats.py
+++ scripts/get_task_stats.py
@@ -1,9 +1,9 @@
 import ray
 import sys
+import os
+import errno
 
 redis_address = sys.argv[1]
-# task_runtime_file = sys.argv[2]
-# worker_num_tasks_run_file = sys.argv[3]
 
 num_tasks = 10000000
 ray.init(redis_address=redis_address)
@@ -29,12 +29,18 @@ for task_profile in task_profiles.items():
     task_durations.append(task_profile[1]["store_outputs_end"] -
                           task_profile[1]['get_arguments_start'])
 
-with open("overall_worker_task_runtime.txt", "w+") as file_handle:
+if not os.path.exists(os.path.dirname("stats/")):
+    try:
+        os.makedirs(os.path.dirname("stats/"))
+    except OSError as exc: # Guard against race condition
+        if exc.errno != errno.EEXIST:
+            raise
+
+with open("stats/overall_worker_task_runtime.txt", "w") as file_handle:
     file_handle.write("\n".join(map(str, list(overall_worker_task_runtime.values()))))
 
-with open("task_runtime_output.txt", "w+") as file_handle:
+with open("stats/task_runtime_output.txt", "w") as file_handle:
     file_handle.write("\n".join(map(str, task_durations)))
 
-
-with open("worker_task_allocation_output.txt", "w+") as file_handle:
+with open("stats/worker_task_allocation_output.txt", "w") as file_handle:
     file_handle.write("\n".join(map(str, list(worker_num_tasks_run.values()))))
