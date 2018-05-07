7a8353ebf0a0725693e7216fb87ec889007ab176
raina4 <raina4@illinois.edu>
2018-05-06 20:35:36 -0500

    added percentile information

--- scripts/plot_worker_task_runtime.py
+++ scripts/plot_worker_task_runtime.py
@@ -9,8 +9,11 @@ with open(worker_utilization_file_name, "r") as file_handle:
 
 mean = np.mean(data)
 std_dev = np.std(data)
+per_90 = np.percentile(data, 90)
+per_95 = np.percentile(data, 95)
+per_99 = np.percentile(data, 99)
 
-print "Mean="+str(mean)+" Std_dev="+str(std_dev)
+print "Mean="+str(mean)+" Std_dev="+str(std_dev)+" 90th percentile="+str(per_90)+" 95th percentile="+str(per_95)+" 99th percentile="+str(per_99)
 
 indexes = np.arange(1, len(data)+1)
 labels = ["Worker " + i for i in map(str, indexes)]
